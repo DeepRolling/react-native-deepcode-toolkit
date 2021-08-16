3d98b19cecad14dfb1070aa677a8106134aba576
baixuanyu <bai.xuan.yu@finsiot.com>
2021-08-16 09:36:53 +0800

    add animation ppo-window and add some hooks

--- src/mobx/mobxHook.ts
+++ src/mobx/mobxHook.ts
@@ -1,11 +1,29 @@
-import { DependencyList, useEffect } from 'react';
-import {autorun} from 'mobx';
+import { DependencyList, MutableRefObject, useEffect, useRef } from 'react';
+import { autorun } from 'mobx';
 
 export function useAutoRun(action: () => void, deps?: DependencyList) {
   useEffect(
     () => {
       let persistenceDataDisposer = autorun(() => {
         action();
+      });
+      return () => {
+        persistenceDataDisposer();
+      };
+    },
+    deps === undefined ? [] : deps //eslint-disable-line react-hooks/exhaustive-deps
+  );
+}
+
+export function useAutoRunWithFirstTimeRef(
+  action: (ref: MutableRefObject<boolean>) => void,
+  deps?: DependencyList
+) {
+  let firstTimeRef = useRef<boolean>(true);
+  useEffect(
+    () => {
+      let persistenceDataDisposer = autorun(() => {
+        action(firstTimeRef);
       });
       return () => {
         persistenceDataDisposer();
